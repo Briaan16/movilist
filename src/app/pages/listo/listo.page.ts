65f802e74cb55cd231571889585179d168146ac8
CETECOM <cetecom_conce@duoc.cl>
2024-10-07 12:22:37 -0300

    api hora, ta fea :,v

--- src/app/pages/listo/listo.page.ts
+++ src/app/pages/listo/listo.page.ts
@@ -9,8 +9,23 @@ import { Hora} from 'src/app/interfaces/horafecha'
 })
 export class ListoPage implements OnInit {
 
-  horas:Hora[]=[];
-
+  horas:Hora ={
+    utc_offset: '',
+    timezone: '',
+    day_of_week: 0,
+    day_of_year: 0,
+    datetime: '',
+    utc_datetime: '',
+    unixtime: 0,
+    raw_offset: 0,
+    week_number: 0,
+    dst: false,
+    abbreviation: '',
+    dst_offset: 0,
+    dst_from: '',
+    dst_until: '',
+    client_ip: '',
+  }
 
   constructor(private dataService: DataserviceService) { }
 
@@ -18,8 +33,9 @@ export class ListoPage implements OnInit {
     console.log("On Init");
     this.dataService.getHora().subscribe(datos =>{
       console.log(datos);
+      this.horas= datos;
       console.log("MI (HORARIO/FECHA)");
-      console.log(this.horas);
+      console.log(this.horas);  
     })
   }
 
