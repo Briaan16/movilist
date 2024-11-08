5b5fc529318bf17bdde038789d4fb3e2368d46eb
Brian <bria.chavez@duocuc.cl>
2024-11-08 16:00:42 -0300

    fayer fayer tatata2

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/compat/firestore';
+import { getFirestore, setDoc, doc} from '@angular/fire/firestore';
 
 
 @Injectable({
@@ -6,7 +8,14 @@ import { Injectable } from '@angular/core';
 })
 export class FirebaseService {
 
-  
+  firestore = inject(AngularFirestore);
 
   constructor() { }
+
+
+
+  //base de datos firebase
+  setDocument(path: string, data: any){
+
+  } 
 }
