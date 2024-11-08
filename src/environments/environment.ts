ca8077d32c5a6da25ef417a1b3f407c361427af5
Brian <bria.chavez@duocuc.cl>
2024-11-08 15:51:24 -0300

    fayer fayer tatata

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyAdA_eRKcEdA8Q4ro7PcM9CpqVKL_rIbas",
+    authDomain: "registrapp-d238a.firebaseapp.com",
+    projectId: "registrapp-d238a",
+    storageBucket: "registrapp-d238a.firebasestorage.app",
+    messagingSenderId: "580216273408",
+    appId: "1:580216273408:web:9a756c26c854be5943f6e5"
+  }
 };
 
 /*
